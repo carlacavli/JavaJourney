ab4423bc20bbe6c0bccf511b13e3faf41a089247
muberracavli <muberracav@hotmail.com>
2019-10-30 11:08:58 -0400

    day17

---
+++ JavaClass/src/Day18/StockProblemHomework.java
@@ -0,0 +1,25 @@
+package Day18;
+
+public class StockProblemHomework {
+    public static void main(String[] args) {
+        // input : [7, 6, 4, 3, 1]
+        // output : 0
+        // explanation : in this case no transaction is done , i.e max profit = 0.
+
+        int [] prices = new int[]{7, 6,4,3,1};
+        int min = Integer.MIN_VALUE;
+        int maxProfit = 0;
+        for (int p : prices) {
+            min = Math.min(min, p);
+            int profit = p - min;
+            maxProfit = Math.max(maxProfit, profit);
+        }
+        System.out.println( "max profit is:  " + maxProfit );
+
+
+
+
+
+
+    }
+}
