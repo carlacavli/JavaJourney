021561bd797624e67955f51a7c0f8cb5c3955188
muberracavli <muberracav@hotmail.com>
2019-11-01 16:03:58 -0400

    FibSeriesHomework

--- JavaClass/src/Day18/FibSeries2.java
+++ JavaClass/src/Day18/FibSeries2.java
@@ -8,15 +8,29 @@ public class FibSeries2 {
         // a b c
         //   a b c
         //     a b c
-        int a = 1;
-        int b = 1;
-        int c;
+//        int a = 1;
+//        int b = 1;
+//        int c;
+//        for (int i = 0; i < 10; i++) {
+//            c = a + b;
+//            a = b;
+//            b = c;
+//            System.out.println(c);
+
+        int fib [] = new int[10];
+        fib[0] = 1;
+        fib[1] = 1;
+        for (int i = 0; i < 8; i++) {         // 8 because i gave already first two numbers
+            fib[i +2] = fib[i +1] + fib[i];
+
+        }
         for (int i = 0; i < 10; i++) {
-            c = a + b;
-            a = b;
-            b = c;
-            System.out.println(c);
+            System.out.println(fib[i]);
+            
         }
 
+
+        }
+
+
     }
-}
