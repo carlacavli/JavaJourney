bb1756c3fd1dd60faf0ef7df465bcfbb905c18a1
muberracavli <muberracav@hotmail.com>
2019-11-09 16:21:43 -0500

    day21

---
+++ JavaClass/src/Day21/JavaMethod1.java
@@ -0,0 +1,16 @@
+package Day21;
+
+
+public class JavaMethod1 {
+    public static void main(String[] args) {
+        AddTwoNumber(4,56);
+
+    }
+    public static void AddTwoNumber(int x, int y){
+        x=4;
+        y=56;
+        int sum;
+        System.out.println("sum="+ (x+y));
+    }
+    }
+
---
+++ JavaClass/src/Day21/JavaMethod3.java
@@ -0,0 +1,14 @@
+package Day21;
+
+public class JavaMethod3 {
+    public class Vehicle {
+        public int doors;
+        public int speed;
+        public String color;
+
+        public void run() {
+            System.out.println("I am running");
+        }
+    }
+
+}
