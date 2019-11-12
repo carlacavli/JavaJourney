b738b39fb9913a00fc01f0c9560dfed43ae08c80
muberracavli <muberracav@hotmail.com>
2019-11-12 16:56:20 -0500

    homestudy

---
+++ JavaClass/src/EvdeCalisma1/EvdeCalisma6.java
@@ -0,0 +1,36 @@
+package EvdeCalisma1;
+
+
+public class EvdeCalisma6 {
+//    public static int toplama(int a, int b, int c ){
+//        return(a+b+c);
+//    }
+//    public static void main(String[] args) {
+//        System.out.println("fonksiyondan donen deger: "+toplama(3,4,5));
+
+
+
+    public static int dortcikar(int a) {
+        return a - 4;
+    }
+    public static int ikicarp(int a) {
+        return a * 2;
+    }
+    public static int ikitopla (int a){
+        return a + 2;
+    }
+    public static void main(String[] args) {
+
+        System.out.println("sonuc:" +dortcikar(ikicarp(ikitopla(5))));
+
+
+
+    }
+
+
+
+
+
+
+    }
+
---
+++ JavaClass/src/EvdeCalisma1/Test.java
@@ -0,0 +1,51 @@
+package EvdeCalisma1;
+
+public class Test {
+    public static void main(String[] args) {
+//        int[][] prices = {
+//                {1, 1, 1},
+//                {1, 1, 1},
+//                {1, 1, 1},
+//                {1, 1, 1},
+//                {10, 20, 40}
+//        };
+//
+//        System.out.println("Average price of items");
+//        for (int i = 0; i < prices.length; i++) {
+//            int sum = 0;
+//            for (int j = 0; j < prices[i].length; j++) {
+//                sum += prices[i][j]; // sum = sum + prices[i][j];
+//            }
+//            double avg = (double) sum / prices[i].length;
+//            System.out.println(i + ". item avg: " + avg);
+//        }
+//        System.out.println("Sum price of markets");
+//        for (int i = 0; i < prices[i].length; i++) {
+//            int sum = 0;
+//            for (int j = 0; j < prices.length; j++) {
+//                sum += prices[j][i]; // sum = sum + prices[i][j];
+//            }
+//            System.out.println(i + ". market sum: " + sum);
+//
+//
+//        int [] []  prices = {{3,5,3},{2,3,1}};
+//        for (int i = 0; i <prices.length ; i++) {   // bu for row icin
+//            for (int j = 0; j <prices[i].length ; j++) {  // bu for column icin
+//                System.out.print(prices[i][j]);
+//
+//            }
+//            System.out.println();
+
+        }
+        String[] [] names = {
+                {"ayse","fatma"},
+                {"candan","balkis"}
+        };
+        
+
+
+        }
+
+
+
+
