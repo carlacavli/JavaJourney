14a90b44dee1b19497c6a3e1cf09e99a6f0e7413
muberracavli <muberracav@hotmail.com>
2019-11-12 16:54:54 -0500

    homestudy

---
+++ JavaClass/src/EvdeCalisma1/Calisma.java
@@ -0,0 +1,142 @@
+package EvdeCalisma1;
+
+import java.util.Scanner;
+
+public class Calisma {
+    public static void main(String[] args) {
+//
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("sayi gir");
+//        double a =scan.nextDouble();
+//        System.out.println("degeri " + a);
+//
+//        int x = 10;
+//        int y = 4;
+//        System.out.println("sonuc "+ (x+y));
+//        System.out.println("sonuc " + ((double)x/y));// double i x yada y nin onune koydugunda sonuc degismez yine 2.5 verir
+//        System.out.println("sonuc" + ((double)(x/y))); // dpuble i buraya koyarsak sonucu double olarak verir 2.0 verir
+
+//        int a = 10;
+//        System.out.println("a'nin degeri" + a--); // eksi isaretlerini sag tarafa koydugumuzda ikinci asamada azaltiyor =10
+//        System.out.println("a'nin degeri"+ a);   // ikinci asamada azalttigi icin burda a 9 oluyor
+
+//        System.out.println("a nin degeri"+ --a); // eksiyi soluna koyunca ilk islemde azaltma yapiyor =9
+//        System.out.println("a'nin degeri"+ a);  // burdada a nin degeri 9 olarak kaliyor
+// ------------------------------------------------------------------
+          /*
+          karsilastirma operatorleri;
+          a==b -->  esitmi ( esitse true esit degilse false)
+          a!=b --> esit degilse ( esit degilse true esitse false)
+          a>b ---> a b den buyukmu , buyukse true degilse flase
+          a<b --->  a b den kucukmu, kucukse true degilse false
+          a<=b ---> a b ye esit veya b den kucuk mu ,oyleyse true degilse false
+          a >=b ---> a b ye esit veya b den buyuk mu , oyleyse true degilse false
+          if (kosul) {
+          if blogu
+          burada kosul saglaninca islemler yapiliyor
+          }
+
+        */
+
+//
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("notu giriniz :");
+//        int note = scan.nextInt();
+//        if(note > 70){
+//            System.out.println("dersten gectiniz ");
+//        }else{
+//            System.out.println("kaldiniz");
+//        }
+// -----------------------------------------------------------------
+        /*
+        ! (Not )
+        kullanimi : !(3>4)
+        anlami : true bir degeri flase , false degeride true
+
+        && ( And )
+        kullanimi : ( 3 > 4 ) && ( "yazilim " == "yazilim")
+        anlami : true , true --> true
+                 false , true --> false
+                 true , false --> false
+                 false ,false --> false
+
+        || (or)
+        kullanimi : ( 3>4 ) || ("yazilim"== "yazilim")
+        anlami : true , true --> true
+                 false , true --> true
+                 true , false --> true
+                 false, false --> false
+
+         */
+//        System.out.println("sonuc " + !(3>4)); //sonuc true cikar cunku parantez icindeki islem true
+
+//        String username = "technostudy";
+//        String password = "12345";
+//        Scanner scan= new Scanner(System.in);
+//        System.out.println("enter username");
+//        String  user_username = scan.nextLine();
+//        System.out.println("enter password");
+//        String pasword = scan.nextLine();
+//
+//        if(!( user_username.equals(username))&& !(password.equals(pasword))){
+//            System.out.println("wrong username and password");
+//
+//        }
+//        else if (( user_username.equals(username))&& !(password.equals(pasword))){
+//            System.out.println("wrong password");
+//
+//        }
+//        else if (!( user_username.equals(username))&& (password.equals(pasword))){
+//            System.out.println(" wrong username");
+//
+//        }else{
+//            System.out.println("complite");
+//       }
+//    ----------------------------------------------------
+
+        // switch - case durumlari ;
+//
+//        Scanner scan = new Scanner(System.in);
+//        int bakiye = 1000;
+//        int islem;
+//        System.out.println("1.bakiye goruntule ");
+//        System.out.println("2. para yatirma");
+//        System.out.println("3.para cekme");
+//        System.out.println("sistemden cikis");
+
+//        islem =scan.nextInt();
+//
+//        switch (islem){
+//            case 1 :
+//                System.out.println("bakiyeniz "+ bakiye +  "tl dir");
+//                break;
+//            case 2:
+//                System.out.println("ne kadar yatiracaksiniz");
+//                int miktar = scan.nextInt();
+//                bakiye += miktar;
+//                System.out.println("bakiyeniz "+ bakiye + " tl dir");
+//                break;
+//            case 3:
+//                System.out.println("ne kadar cekeceksiniz");
+//                miktar =scan.nextInt();
+//                bakiye -= miktar;
+//                System.out.println("bakiyeniz "+ bakiye + " tl dir");
+//                break;
+//            case 4:
+//                System.out.println("sistemden cikiliyor");
+//                break;
+//            default:
+//                System.out.println("gecersiz islem");        // yetersiz bakiye kismini yazamadim
+//        }
+
+
+
+
+
+
+        }
+
+
+
+
+    }
