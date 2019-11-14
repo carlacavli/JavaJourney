680e57ca309a38482cdfddf26c358175f285d312
muberracavli <muberracav@hotmail.com>
2019-11-14 16:18:16 -0500

    AllHomeworks

---
+++ JavaClass/src/Reversed/ReverseHomework.java
@@ -0,0 +1,15 @@
+package Reversed;
+
+public class ReverseHomework {
+    public static void main(String[] args) {
+
+        int num = 4789, reversed = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        System.out.println("Reversed Number: " + reversed);
+    }
+}
+
