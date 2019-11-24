f3c0517788e015af75d83a4ff55816e1556fd189
muberracavli <muberracav@hotmail.com>
2019-11-24 15:20:42 -0500

    day35

---
+++ JavaClass/src/Day35/task/Bank.java
@@ -0,0 +1,44 @@
+package Day35.task;
+
+public class Bank {
+       int year ;
+       int serial;
+       String name;
+       boolean trustworthiness;
+
+        public Bank() {
+            year = 0;
+            serial = 0;
+            name = "Unknown bank";
+        }
+
+        public Bank(boolean t) {
+            this(2019, 10, "Bank of America");
+            trustworthiness = t;
+        }
+
+        public Bank(int y, int num, String name) {
+            this.year = y;
+            this.name = name;
+
+            serial = num;
+        }
+
+        public Bank(int year, int num, String name, boolean t) {
+            this.year = year;
+            serial = num;
+            this.name = name;
+            trustworthiness = t;
+
+        }
+
+        public String printOut() {
+            String a = "Bank Year:" + year + "\n";
+            String b = "Bank Name:" + name + "\n";
+            String c = "Serial Number:" + serial + "\n";
+            String d = "Trustworthiness:" + trustworthiness;
+            return a + b + c + d;
+        }
+
+    }
+
---
+++ JavaClass/src/Day35/task/Task2.java
@@ -0,0 +1,33 @@
+package Day35.task;
+
+import Day35.task.Bank;
+
+public class Task2 {
+
+    //create a class
+// add at least 3 attributes
+// add at least 3 constructors
+// add method that return all attributes as one string
+// in Task 2 class create at least 4 object on that class
+// call method to get attributes and print it
+
+        public static void main(String[] args) {
+            Bank chase = new Bank(2010, 1, "Chase");
+            System.out.println(chase.printOut());
+            System.out.println("------------------------------------");
+
+            Bank bank = new Bank();
+            bank.trustworthiness = true;
+            bank.name = " some name";
+            bank.year = 210;
+            System.out.println(bank.printOut());
+            System.out.println("------------------------------------");
+
+            Bank trustworthyBank = new Bank(true);
+            System.out.println(trustworthyBank.printOut());
+            System.out.println("------------------------------------");
+
+            Bank td = new Bank(2005, 101010, "TD", true);
+            System.out.println(td.printOut());
+        }
+}
