85d7eb81238e8a3be3223481588ad01c44fa1e43
muberracavli <muberracav@hotmail.com>
2019-11-24 15:21:43 -0500

    day36

---
+++ JavaClass/src/Day36/Examples/Exception1.java
@@ -0,0 +1,12 @@
+package Day36.Examples;
+
+
+    public class Exception1 {
+        // Java program to demonstrate how exception is thrown.
+        public static void main(String args[]) {
+
+            String str = null;
+            System.out.println(str.length());
+
+        }
+    }
---
+++ JavaClass/src/Day36/Examples/Exception4.java
@@ -0,0 +1,18 @@
+package Day36.Examples;
+
+import java.io.File;
+import java.io.FileReader;
+
+public class Exception4 {
+    public static void main(String[] args) {
+        try {
+            File file = new File("D:\\SomeFolder\\file.txt");
+
+            FileReader fileReader = new FileReader(file);
+        } catch (Exception ex) {
+            System.out.println("File not found;");
+            System.out.println(ex.getStackTrace());
+        }
+    }
+}
+
