519f8b99ece661b317d14a26f8af58270bf2d3ab
muberracavli <muberracav@hotmail.com>
2019-11-26 21:01:26 -0500

    Day27

---
+++ JavaClass/src/Day27/ArrayListExample.java
@@ -0,0 +1,23 @@
+package Day27;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        String[] s2 = new String[2];
+        s2[0] = "apple";
+        s2[1] = "lemon";
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("apple");
+        list.add("lemon");
+        System.out.println(list);
+
+        String[] s22 = {"apple", "lemon"};
+        List<String> list2 = Arrays.asList("apple", "lemon");
+        System.out.println(list2);
+    }
+}
+
