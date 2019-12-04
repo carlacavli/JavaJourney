c939f5ec3eeb1c268db87415e0ef21363d7a9788
muberracavli <muberracav@hotmail.com>
2019-12-03 21:19:23 -0500

    Abstract

---
+++ JavaClass/src/Day44/Differences_compandruntime_Oop/Abstraction.java
@@ -0,0 +1,54 @@
+package Day44.Differences_compandruntime_Oop;
+
+public class Abstraction {
+
+    /*
+    key word is : must be inheritad !!
+    abstraction involves the facility to define objects that represent abstract “actors” that can perform work,
+    report on and change their state, and “communicate” with other objects in the system.
+
+     In simplest words,
+     you can define abstraction as which captures only those details about a Java object that are relevant to the
+      current perspective.
+
+      Types of abstraction !!
+
+  1. Data abstraction;
+  Data abstraction is the way to create complex data types and exposing only meaningful operations to interact with the data type,
+  whereas hiding all the implementation details from outside works.
+  The benefit of this approach involves capability of improving the implementation over time
+  e.g. solving performance issues is any. The idea is that such changes are not supposed to have any impact on
+  client code since they involve no difference in the abstract behavior.
+
+   2. Control abstraction;
+  A software is essentially a collection of numerous statements written in any programming language. Most of the times, statement are similar
+  and repeated over places multiple times.
+
+  Control abstraction is the process of identifying all such statements and expose them as a unit of work.
+  We normally use this feature when we create a function to perform any work.
+
+
+     */
+    /*
+
+    Abstraction simply means separating responsibilities between different modules.
+    1.Is one which make a class abstract in object orient programming. It means it provides only essential features for a certain program when needed.
+    2. Abstract class cannot be instantiated.
+    3.Abstract methods will be declared in parent class.
+    4.If a class has abstract method .Its class should also been declared as abstract.
+     */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
